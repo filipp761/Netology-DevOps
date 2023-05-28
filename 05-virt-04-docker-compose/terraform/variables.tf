4bc4e576a9f70e053f176b5855ee38e429ba5656
Philipp Kopylov <58370923+filipp761@users.noreply.github.com>
2023-05-28 17:25:56 +0300

    Create variables.tf

---
+++ 05-virt-04-docker-compose/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1ghu94q4h4fj60hlsag"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gs7qhv4u13mhqpm6oa"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8fkekp57hdsmg3ujse"
+}
