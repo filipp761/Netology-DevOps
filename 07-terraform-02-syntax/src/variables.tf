4194dca55a204d1fcaba5b1230e83437227fa08b
Philipp Kopylov <58370923+filipp761@users.noreply.github.com>
2023-06-17 11:30:28 +0300

    Create variables.tf

---
+++ 07-terraform-02-syntax/src/variables.tf
@@ -0,0 +1,67 @@
+###cloud vars
+variable "token" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth->
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "default_cidr" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "develop"
+  description = "VPC network & subnet name"
+}
+
+variable "vm_web_image" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "ubuntu release name"
+}
+
+variable "vm_web_instance" {
+  type        = string
+  default     = "netology-develop-platform-web"
+  description = "instance name"
+}
+
+variable "vm_db_instance" {
+  type        = string
+  default     = "netology-develop-platform-db"
+  description = "instance name"
+}
+
+variable "vm_web_resource" {
+  type        = map(number)
+  description = "resource"
+  default = {
+        core = 2,
+        memory = 2,
+        core_fraction = 20
+        }
+}
+###ssh vars
+
+variable "vms_ssh_root_key" {
+  type        = string
+  default     = "..."
+  description = "ssh-keygen -t ed25519"
+}
