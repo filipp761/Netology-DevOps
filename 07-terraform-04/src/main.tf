cc9fb221489dac37d6dd6386870db01cf5b5b5db
Philipp Kopylov <58370923+filipp761@users.noreply.github.com>
2023-06-26 21:04:23 +0300

    Update main.tf

--- 07-terraform-04/src/main.tf
+++ 07-terraform-04/src/main.tf
@@ -1,9 +1,6 @@
-resource "yandex_vpc_network" "develop" {
-  name = var.vpc_name
-}
-resource "yandex_vpc_subnet" "develop" {
-  name           = var.vpc_name
-  zone           = var.default_zone
-  network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = var.default_cidr
+module "vpc_dev" {
+  source       = "./vpc"
+  env_name     = "develop"
+  zone = "ru-central1-a"
+  cidr = "10.0.1.0/24"
 }
