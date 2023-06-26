94e03d3e79f81a5b1c05f459f79d68edc859d561
Philipp Kopylov <58370923+filipp761@users.noreply.github.com>
2023-06-26 22:42:37 +0300

    Update main.tf

--- 07-terraform-04/src/vpc/main.tf
+++ 07-terraform-04/src/vpc/main.tf
@@ -1,6 +1,16 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">=0.13"
+}
+
 resource "yandex_vpc_network" "develop" {
   name = var.vpc_name
 }
+
 resource "yandex_vpc_subnet" "develop" {
   name           = var.vpc_name
   zone           = var.default_zone
