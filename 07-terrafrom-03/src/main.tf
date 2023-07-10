e627b22630149dba23492a2b23103507d08138c6
Philipp Kopylov <58370923+filipp761@users.noreply.github.com>
2023-07-10 22:10:28 +0300

    Update main.tf

--- 07-terrafrom-03/src/main.tf
+++ 07-terrafrom-03/src/main.tf
@@ -7,3 +7,14 @@ resource "yandex_vpc_subnet" "develop" {
   network_id     = yandex_vpc_network.develop.id
   v4_cidr_blocks = var.default_cidr
 }
+
+resource "local_file" "hosts_cfg" {
+  content = templatefile("${path.module}/hosts.tftpl", 
+    { 
+      webservers = yandex_compute_instance.platform
+      databases = yandex_compute_instance.databases
+      storage = yandex_compute_instance.storage
+     })
+
+  filename = "${abspath(path.module)}/hosts.cfg"
+}
