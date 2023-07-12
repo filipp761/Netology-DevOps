b92eb90b9ce35f6c01d2d58b51e233bfc7999d8b
Philipp Kopylov <58370923+filipp761@users.noreply.github.com>
2023-07-12 21:47:47 +0300

    Update output.tf

--- 07-terrafrom-03/src/output.tf
+++ 07-terrafrom-03/src/output.tf
@@ -1,18 +1,18 @@
 output  "webserver" {
   value = [for i in  yandex_compute_instance.platform: 
-  "name = ${i.name}, id = ${i.id},  fqdn = ${i.fqdn}" ]
+  "name = ${i.name},\nid = ${i.id},\nfqdn = ${i.fqdn}" ]
 }
 
 output "databases" {
   value = [
     for i in  yandex_compute_instance.databases: 
-   "name = ${i.name}, id = ${i.id}, fqdn = ${i.fqdn}"
+   "name = ${i.name},\nid = ${i.id},\nfqdn = ${i.fqdn}"
   ]
 }
 
 output "storage" {
   value = [
     for i in  yandex_compute_instance.storage.*: 
-   "name = ${i.name}, id = ${i.id}, fqdn = ${i.fqdn}"
+   "name = ${i.name},\nid = ${i.id},\nfqdn = ${i.fqdn}"
   ]
 }
