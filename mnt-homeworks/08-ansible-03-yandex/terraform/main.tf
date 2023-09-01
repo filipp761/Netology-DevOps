7eabf696108d221a7dc3870fc05210b7128275a1
Philipp Kopylov <58370923+filipp761@users.noreply.github.com>
2023-09-01 13:51:34 +0300

    Create main.tf

---
+++ mnt-homeworks/08-ansible-03-yandex/terraform/main.tf
@@ -0,0 +1,18 @@
+resource "yandex_vpc_network" "develop" {
+  name = var.vpc_name
+}
+resource "yandex_vpc_subnet" "develop" {
+  name           = var.vpc_name
+  zone           = var.default_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr
+}
+
+resource "local_file" "hosts_cfg" {
+  content = templatefile("${path.module}/hosts.tftpl", 
+    { 
+      logs = yandex_compute_instance.logs
+     })
+
+  filename = "${abspath(path.module)}/ansible/inventory/prod.yml"
+}
